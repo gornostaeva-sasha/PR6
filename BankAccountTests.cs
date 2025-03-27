9083b6f1a6e7e7a6535ba0faed56a2d818e8bb25
gornostaeva-sasha <alexandra.marmyshkina@yandex.ru>
2025-03-27 12:44:14 +0300

    Update BankAccountTests.cs

--- BankAccountTests.cs
+++ BankAccountTests.cs
@@ -1,14 +1,94 @@
-﻿using BankAccountNS;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+using System;
 
-namespace BankTests
+namespace BankAccountTestss
 {
     [TestClass]
     public class BankAccountTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
         {
+            
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Roman Abramovich", beginningBalance);
+
+           
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                
+                StringAssert.Contains(ex.Message, BankAccount.DebitAmountLessThanZeroMessage);
+                return;
+            }
+
+            Assert.Fail("Ожидаемое исключение не было выброшено.");
+        }
+
+        [TestMethod]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+           
+            double beginningBalance = 11.99;
+            double debitAmount = 20.0;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+              
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+                return;
+            }
+
+            Assert.Fail("Ожидаемое исключение не было выброшено.");
+        }
+
+        [TestMethod]
+        public void Credit_WhenAmountIsNegative_ShouldThrowArgumentOutOfRange()
+        {
+            
+            double beginningBalance = 100.0;
+            double creditAmount = -50.0;
+            BankAccount account = new BankAccount("Mr. Credit Tester", beginningBalance);
+
+           
+            try
+            {
+                account.Credit(creditAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+             
+                StringAssert.Contains(e.Message, BankAccount.CreditAmountIsNegativeMessage);
+                return;
+            }
+
+            Assert.Fail("Ожидаемое исключение не было выброшено.");
+        }
+
+        [TestMethod]
+        public void Credit_WhenAmountIsValid_ShouldIncreaseBalance()
+        {
+            
+            double beginningBalance = 100.0;
+            double creditAmount = 50.0;
+            BankAccount account = new BankAccount("Mr. Credit Tester", beginningBalance);
+
+           
+            account.Credit(creditAmount);
+
+           
+            Assert.AreEqual(150.0, account.Balance);
         }
     }
 }
